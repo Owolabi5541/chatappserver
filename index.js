fbde932e66ddf44be775f75d050492981529851f
Owolabi Idris Yusuf <owolabiidris4@gmail.com>
2023-09-21 10:24:57 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    
+    try{
+        const r = await axios.put(
+            'https://api.chatengine.io/users/',
+            { username: username, secret: username, first_name: username},
+            { headers: { "private-key": "e5462aad-f6a9-43a5-9dcb-4ba13e1504ce" } }
+        );
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+        }
+});
+
+app.listen(3001);
